abd6e925959681fa053d6d78020d7d36bf1fbb96
Maksim Lukin <maximlm93@gmail.com>
2025-02-21 17:28:19 +0500

    initial commit

---
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package hexlet.code.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+    @GetMapping(path = "")
+    public String welcomePage() {
+        return "Welcome to Spring";
+    }
+}
